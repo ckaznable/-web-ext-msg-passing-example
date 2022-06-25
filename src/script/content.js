021688687b577b13680fea7ea653e36722c183b9
John <kami25565@gmail.com>
2022-06-25 12:01:38 +0800

    update v0.0.4 example

--- src/script/content.js
+++ src/script/content.js
@@ -1,5 +1,13 @@
-import { sendWithResponse } from "web-ext-msg-passing"
+import { sendWithResponse, installListener, listen } from "web-ext-msg-passing"
 
 (async () => {
+  // listen content event
+  installListener()
+  listen("getContentHtml", (_, reply) => {
+    console.log("getContentHtml")
+    reply(document.documentElement.innerHTML)
+  })
+
+  // get data from bg
   console.log(await sendWithResponse("get google html"))
 })()
--- src/script/options.js
+++ src/script/options.js
@@ -1,9 +1,13 @@
-import { send } from "web-ext-msg-passing"
+import { send, sendToContent } from "web-ext-msg-passing"
 
-document.getElementById("open").addEventListener("click", async (e) => {
+document.getElementById("open").addEventListener("click", () => {
   send("openTab")
 })
 
-document.getElementById("close").addEventListener("click", e => {
+document.getElementById("close").addEventListener("click", () => {
   send("closeTab")
+})
+
+document.getElementById("tstc").addEventListener("click", async () => {
+  console.log(await sendToContent("getContentHtml"))
 })
